a84c34055ae1c1141829f5064d5f6d26517cfbd3
Spinelli666 <bernardospinelli@hotmail.com.br>
2025-02-17 17:09:22 -0300

    (feat): Realizando Login

--- 5_auth/auth.ts
+++ 5_auth/auth.ts
@@ -1,10 +1,10 @@
 import type { NextAuthConfig } from "next-auth";
 import NextAuth from "next-auth";
 
-import Google from "next-auth/providers/google";
+import google from "next-auth/providers/google";
 
 const config = {
-    providers: [Google],
+    providers: [google],
 } satisfies NextAuthConfig;
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
---
+++ 5_auth/src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import { auth, signIn, signOut } from "auth"
+import Link from "next/link"
+
+const Navbar = async () => {
+
+    const session = await auth()
+
+  return (
+    <div>
+      <Link href='/'>Home</Link>
+      <div>
+        {session && session.user ? (
+            // logado
+            <div>
+                {/* login social => name, email, image */}
+                <p>{session.user.name}</p>
+            </div>
+        ) : (
+            // delogado
+            <form action={async () => {
+                'use server'
+                await signIn()
+            }}>
+                <button>Entrar</button>
+            </form>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar

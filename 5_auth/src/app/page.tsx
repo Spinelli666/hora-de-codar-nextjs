c5e97414c5cad871e12801089e6fb8a71db17624
Spinelli666 <bernardospinelli@hotmail.com.br>
2025-02-18 14:25:34 -0300

    (feat): Protegendo Páginas do Servidor

--- 5_auth/src/app/page.tsx
+++ 5_auth/src/app/page.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <Link href='/client'>Página com client component</Link>
+      <Link href='/server'>Página com server component</Link>
     </main>
   );
 }
